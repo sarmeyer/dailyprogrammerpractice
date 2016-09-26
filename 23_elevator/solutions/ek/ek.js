06ac3d3b37cbfd57c54dc5cd974b80943e41820c
Sarah <sarahmichellemeyer@gmail.com>
2016-09-26 12:50:50 -0600

    solution to counting letters.

--- 23_elevator/solutions/ek/ek.js
+++
@@ -1,9 +0,0 @@
-// Created by Elana Kopelevich
-
-$(document).ready(function(){
-  var id = 1;
-  for (var i = 0; i < 33; i++) {
-    $('.btn-container').append('<div class="btn"><h1 class="num">' + id + '</h1></div>');
-    id++;
-  }
-});
