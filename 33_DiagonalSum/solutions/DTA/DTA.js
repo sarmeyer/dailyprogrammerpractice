80615ae2842a2e4384e1dc51983336996204c0b4
Sarah <sarahmichellemeyer@gmail.com>
2016-09-27 09:36:13 -0600

    added semi-colons

--- 33_DiagonalSum/solutions/DTA/DTA.js
+++ 33_DiagonalSum/solutions/DTA/DTA.js
@@ -2,17 +2,17 @@
 module.exports = function diagonalsSum(matrix) {
   if (matrix.length < 2) {
     if (matrix.length === 1) {
-      return matrix[0] * 2
+      return matrix[0] * 2;
     }
-    return 0
+    return 0;
   }
-  var innerDiagonal = 0
-  var outerDiagonal = matrix[0].length - 1
-  var matrixSum = 0
+  var innerDiagonal = 0;
+  var outerDiagonal = matrix[0].length - 1;
+  var matrixSum = 0;
   for (var i = 0; i < matrix.length; i++) {
     matrixSum += matrix[i][innerDiagonal] + matrix[i][outerDiagonal]
-    innerDiagonal++
-    outerDiagonal--
+    innerDiagonal++;
+    outerDiagonal--;
   }
-  return matrixSum
+  return matrixSum;
 }
